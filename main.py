10ef93619f232971d5427bb903d74ec29b1f9f4b
RubixSolver99 <blackbattiger@outlook.com>
2025-11-08 17:58:51 -0600

    Updated INA219_Voltage log file

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ def update_screen():
     \  /\  / (_| | | |  __/ |_/ / (_) | |_ 
     \/  \/ \__,_|_|  \___\____/ \___/ \__|                                       
         """)
-    print("\n\nMAIN DASHBOARD\n\n")
+    print("\n\n")
     print(f"INA219 Voltage: {voltage} V")
 
 print("Starting Main Program...")
@@ -23,6 +23,6 @@ print("Starting Main Program...")
 while True:
     print("Reading INA219 Voltage...")
     voltage = L1_ina.readVolts()
-    log.tmpFile(voltage, "INA219 Voltage")
+    log.tmpFile(voltage, "INA219_Voltage")
     update_screen()
     time.sleep(1)
