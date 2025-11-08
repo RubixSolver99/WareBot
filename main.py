cc6c2b606026a3f9bf216b1cd11bb50e71a854e1
RubixSolver99 <blackbattiger@outlook.com>
2025-11-08 17:47:44 -0600

    Created startup message & voltage output in main

--- main.py
+++ main.py
@@ -3,6 +3,20 @@ import time
 from basics import L1_ina
 from basics import L1_log as log
 
+print("""
+ _    _               ______       _   
+| |  | |              | ___ \     | |  
+| |  | | __ _ _ __ ___| |_/ / ___ | |_ 
+| |/\| |/ _` | '__/ _ \ ___ \/ _ \| __|
+\  /\  / (_| | | |  __/ |_/ / (_) | |_ 
+ \/  \/ \__,_|_|  \___\____/ \___/ \__|                                       
+      """)
+
+print("Starting Main Program...")
+
 while True:
-    log.tmpFile(L1_ina.readVolts(), "INA219 Voltage")
+    print("Reading INA219 Voltage...")
+    voltage = L1_ina.readVolts()
+    print(f"INA219 Voltage: {voltage} V")
+    log.tmpFile(voltage, "INA219 Voltage")
     time.sleep(1)
