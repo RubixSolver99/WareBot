bdfe8f70216e298bd88f52d38aceef138fe987b8
RubixSolver99 <blackbattiger@outlook.com>
2025-11-08 17:52:52 -0600

    Updated main to clear/update dispalyed outputs

--- main.py
+++ main.py
@@ -1,22 +1,28 @@
-import time
+import time, os
 
 from basics import L1_ina
 from basics import L1_log as log
 
-print("""
- _    _               ______       _   
-| |  | |              | ___ \     | |  
-| |  | | __ _ _ __ ___| |_/ / ___ | |_ 
-| |/\| |/ _` | '__/ _ \ ___ \/ _ \| __|
-\  /\  / (_| | | |  __/ |_/ / (_) | |_ 
- \/  \/ \__,_|_|  \___\____/ \___/ \__|                                       
-      """)
+voltage = 0
+
+def update_screen():
+    os.system('cls' if os.name == 'nt' else 'clear')
+    print("""
+    _    _               ______       _   
+    | |  | |              | ___ \     | |  
+    | |  | | __ _ _ __ ___| |_/ / ___ | |_ 
+    | |/\| |/ _` | '__/ _ \ ___ \/ _ \| __|
+    \  /\  / (_| | | |  __/ |_/ / (_) | |_ 
+    \/  \/ \__,_|_|  \___\____/ \___/ \__|                                       
+        """)
+    print("\n\nMAIN DASHBOARD\n\n")
+    print(f"INA219 Voltage: {voltage} V")
 
 print("Starting Main Program...")
 
 while True:
     print("Reading INA219 Voltage...")
     voltage = L1_ina.readVolts()
-    print(f"INA219 Voltage: {voltage} V")
     log.tmpFile(voltage, "INA219 Voltage")
+    update_screen()
     time.sleep(1)
