eb16c501e56f115bfe6aad861d51424615360158
kevingar2 <kevingar2001@gmail.com>
2025-11-08 18:56:49 -0600

    Added extra spacing in main screen

--- telemetry.py
+++ telemetry.py
@@ -11,7 +11,7 @@ def update_all():
     log.tmpFile(voltage, "INA219_Voltage")
 
     # Read Compass Heading
-    heading = round(compass.get_heading(), 0)
+    heading = int(compass.get_heading())
     log.tmpFile(heading, "Compass_Heading")
 
 def get_all():
@@ -20,4 +20,4 @@ def get_all():
         "INA219_Voltage": voltage,
         "Compass_Heading": heading
     }
-    
+
