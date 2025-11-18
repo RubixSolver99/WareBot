a22f1af4fe399eb83772d3e6dd51bc869d4ab68e
RubixSolver99 <blackbattiger@outlook.com>
2025-11-17 18:24:41 -0600

    Added terminate handler

--- main.py
+++ main.py
@@ -1,12 +1,27 @@
-import time, os
+import time, signal, sys
 from multiprocessing import Process
 
 import telemetry, utils, vision
 
+vision_process = None
+
 def vision_worker():
     vision.start_pallet_filter()
 
+def terminate_handler(signum, frame):
+    print("Terminating processes...")
+
+    if vision_process is not None:
+        vision_process.terminate()
+        vision_process.join()
+
+    print("Done.")
+
+    sys.exit(0)
+
 print("Starting Main Program...")
+
+signal.signal(signal.SIGINT, terminate_handler)
 
 vision_process = Process(target=vision_worker)
 vision_process.start()
