57aba4c6c98ba087023bc8df3e3a54c7ecb8a95c
RubixSolver99 <blackbattiger@outlook.com>
2025-11-24 17:15:45 -0600

    Moving servo control to drivetrain (easier for UDP control)

--- drivetrain.py
+++ drivetrain.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-# Drive the SCUTTLE while visualizing a simple LIDAR point cloud in NodeRED
+# Drive the SCUTTLE while receiving commands from NodeRED dashboard
 
 import socket
 import json
@@ -8,7 +8,16 @@ import basics.L2_speed_control as sc
 import time
 from threading import Thread
 
-class DriveTrain:
+
+from gpiozero import Servo as servo                # for forklift servo control
+from gpiozero.pins.pigpio import PiGPIOFactory     # for precise servo control
+
+import time                                        # for keeping time
+
+SERVO_UP_POS = 0.25
+SERVO_DOWN_POS = 0.70
+
+class MotorControl:
 
     def __init__(self):
 
@@ -18,6 +27,12 @@ class DriveTrain:
         self.A_matrix = np.array([[1/self.wheelRadius, -self.wheelBase/self.wheelRadius], [1/self.wheelRadius, self.wheelBase/self.wheelRadius]])
         self.max_xd = 0.4
         self.max_td = (self.max_xd/self.wheelBase)
+
+        #Forklift Servo Setup#
+        factory = PiGPIOFactory()
+        self.forklift_servo_A = servo(24, pin_factory=factory)                   # PIN 18        GPIO24
+        self.forklift_servo_B = servo(25, pin_factory=factory)                   # PIN 22        GPIO25
+        self.forklift_up()
 
         #UPD communication#
         self.IP = "127.0.0.1"
@@ -33,9 +48,10 @@ class DriveTrain:
         self.dashBoardDataThread = Thread(target=self._dashBoardDataLoop, daemon=True)
         self.dashBoardDataThread.start()
 
-        #Driving Thread#
+        #Motor Control Thread#
         self.controlThread = Thread(target=self._controlLoop, daemon=True)
         self.controlThread.start()
+
 
     def _dashBoardDataLoop(self):
         while True:
@@ -50,9 +66,15 @@ class DriveTrain:
         while True:
             if self.dashBoardData != None:
                 try:
-                    userInputTarget = self.dashBoardData['one_joystick']['vector']
+                    self.userInput = self.dashBoardData['one_joystick']
+                    userInputTarget = self.userInput['vector']
                     wheelSpeedTarget = self._getWheelSpeed(userInputTarget)
                     sc.driveOpenLoop(wheelSpeedTarget)
+
+                    if self.dashBoardData['buttons']['A']:
+                        self.forklift_up()
+                    elif self.dashBoardData['buttons']['B']:
+                        self.forklift_down()
                 except: 
                     pass
 
@@ -78,11 +100,23 @@ class DriveTrain:
 
     def getdashBoardData(self):
         return self.dashBoardData
+    
+
+    def forklift_down(self):
+        for i in range(int(SERVO_UP_POS*100), int(SERVO_DOWN_POS*100), 5):
+            pos = i / 100.0
+            self.forklift_servo_A.value = pos
+            self.forklift_servo_B.value = -pos
+            time.sleep(0.05)
+
+    def forklift_up(self):
+        self.forklift_servo_A.value = SERVO_UP_POS
+        self.forklift_servo_B.value = -SERVO_UP_POS
 
 
 if __name__ == "__main__":
 
-    robot = DriveTrain()
+    robot = MotorControl()
     try:
         while True:
             time.sleep(1)
