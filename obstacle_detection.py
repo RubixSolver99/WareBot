bfe3afc0803f2a4a652a114e6b6e739b991362d0
RubixSolver99 <blackbattiger@outlook.com>
2025-12-09 17:36:26 -0600

    Cleaned up file for use in main program

--- obstacle_detection.py
+++ obstacle_detection.py
@@ -11,23 +11,16 @@ from time import sleep
 import time
 from threading import Thread
 
+IP = "127.0.0.1"
+DASHBOARD_PORT = 3555
+
 CARTESIAN_SCALE_X = 1.5    # Scale factor to convert to meters
 CARTESIAN_SCALE_Y = 1.5    # Scale factor to convert to meters
 
 class ObstacleDetection:
 
     def __init__(self):
-
-        #Kinematics#
-        # self.wheelRadius = 0.04
-        # self.wheelBase = 0.1
-        # self.A_matrix = np.array([[1/self.wheelRadius, -self.wheelBase/self.wheelRadius], [1/self.wheelRadius, self.wheelBase/self.wheelRadius]])
-        # self.max_xd = 0.4
-        # self.max_td = (self.max_xd/self.wheelBase)
-
         #UPD communication#
-        self.IP = "127.0.0.1"
-        self.port = 3553
         self.dashBoardDatasock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.dashBoardDatasock.bind((self.IP, self.port))
         self.dashBoardDatasock.settimeout(.25)
@@ -44,20 +37,12 @@ class ObstacleDetection:
         lidarPubThread = Thread(target=self.scan_loop, daemon=True)
         lidarPubThread.start()
 
-        #NodeRED Data Thread#
-        # self.dashBoardDataThread = Thread(target=self._dashBoardDataLoop, daemon=True)
-        # self.dashBoardDataThread.start()
-
-        #Driving Thread#
-        # self.controlThread = Thread(target=self._controlLoop, daemon=True)
-        # self.controlThread.start()
-
     def scan_loop(self):
         start_time = time.time()
         while True:
             data = self.cartesian_scan()
             data_msg = data.encode('utf-8')
-            self.dashBoardDatasock.sendto(data_msg, ("127.0.0.1", 3555))
+            self.dashBoardDatasock.sendto(data_msg, (IP, DASHBOARD_PORT))
             sleep(0.5)
 
     def cartesian_scan(self):
@@ -78,49 +63,6 @@ class ObstacleDetection:
     def format_row(self, point, r=3):
         x, y = point
         return '{x: ' + str(x) + ', y: ' + str(y) + ', r:' + str(r) + '},'
-
-    # def _dashBoardDataLoop(self):
-    #     while True:
-    #         try:
-    #             dashBoardData,recvAddr = self.dashBoardDatasock.recvfrom(1024)
-    #             self.dashBoardData = json.loads(dashBoardData)
-
-    #         except socket.timeout:
-    #             self.dashBoardData = None
-
-    # def _controlLoop(self):
-    #     while True:
-    #         if self.dashBoardData != None:
-    #             try:
-    #                 userInputTarget = self.dashBoardData['one_joystick']
-    #                 wheelSpeedTarget = self._getWheelSpeed(userInputTarget)
-    #                 sc.driveOpenLoop(wheelSpeedTarget)
-    #             except: 
-    #                 pass
-
-    # def _getWheelSpeed(self,userInputTarget):
-    #     try:
-    #         robotTarget = self._mapSpeeds(np.array([userInputTarget['y'],-1*userInputTarget['x']]))
-    #         wheelSpeedTarget = self._calculateWheelSpeed(robotTarget)
-    #         return wheelSpeedTarget
-    #     except:
-    #         pass
-    
-    # def _mapSpeeds(self,original_B_matrix):
-    #     B_matrix = np.zeros(2)
-    #     B_matrix[0] = self.max_xd * original_B_matrix[0]
-    #     B_matrix[1] = self.max_td * original_B_matrix[1]
-    #     return B_matrix
-
-    # def _calculateWheelSpeed(self,B_matrix):
-    #     C_matrix = np.matmul(self.A_matrix,B_matrix)
-    #     C_matrix = np.round(C_matrix,decimals=3)
-    #     return C_matrix
-
-
-    # def getdashBoardData(self):
-    #     return self.dashBoardData
-
 
 if __name__ == "__main__":
 
