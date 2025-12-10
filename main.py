a7264b2831ba810c91e6fdee5c7fcbd426007c4a
RubixSolver99 <blackbattiger@outlook.com>
2025-12-09 18:47:52 -0600

    First attempt at pallet auto align

--- main.py
+++ main.py
@@ -88,7 +88,6 @@ while True:
         # Parse the values
         width, angle = map(float, parts)
         print(f"Pallet Found - Width: {width}, Angle: {angle}")
-        # motor_controller.approach_pallet(width, angle)
-
-
-
+        motor_controller.approach_pallet(width, angle)
+    else:
+        motor_controller.stop()
